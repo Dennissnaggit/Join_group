96217011ca8b4324550a39132744e076dfbbe5f2
Dennis Falkner <dennis.falkner@snaggit.app>
2026-05-20 13:02:03 +0200

    main html gebaut wo die daten übergeben werden

---
+++ script/main.js
@@ -0,0 +1,38 @@
+import { auth, db } from "./firebase.js";
+
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    console.log("Eingeloggt:", user.uid);
+
+    const userRef = doc(db, "users", user.uid);
+    const userSnap = await getDoc(userRef);
+
+    if (userSnap.exists()) {
+      const userData = userSnap.data();
+
+      console.log("Userdaten:", userData);
+
+      // Beispiel: Namen anzeigen
+      document.getElementById("username").textContent = userData.name;
+
+      // Beispiel: E-Mail anzeigen
+      document.getElementById("userEmail").textContent = userData.email;
+
+    } else {
+      console.log("Kein User-Dokument gefunden.");
+    }
+
+  } else {
+    console.log("Nicht eingeloggt");
+    window.location.href = "../index.html";
+  }
+});
